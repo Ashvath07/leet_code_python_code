e57c0f48ddf5db283bb3c9f43ce99ab1ab530835
ASHVATH <ashvathmurali2@gmail.com>
2026-08-31 10:36:01 +0530

    Time: 0 ms (100%), Space: 12.4 MB (52.62%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.py
+++ 0520-detect-capital/0520-detect-capital.py
@@ -1,6 +1,3 @@
 class Solution(object):
     def detectCapitalUse(self, word):
-        if word.isupper() or word.islower() or word.istitle():
-            return True
-        else:
-            return False
+        return word.isupper() or word.islower() or word.istitle()

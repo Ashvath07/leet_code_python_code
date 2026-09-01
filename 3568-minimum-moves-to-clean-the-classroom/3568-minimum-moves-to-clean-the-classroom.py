1f79687cb733d93175bb3ceae930a93cc5eb0ceb
ASHVATH <ashvathmurali2@gmail.com>
2026-09-01 08:13:54 +0530

    Time: 2551 ms (50%), Space: 17.7 MB (58.33%) - LeetHub

--- 3568-minimum-moves-to-clean-the-classroom/3568-minimum-moves-to-clean-the-classroom.py
+++ 3568-minimum-moves-to-clean-the-classroom/3568-minimum-moves-to-clean-the-classroom.py
@@ -2,96 +2,73 @@ from collections import deque
 
 class Solution(object):
     def minMoves(self, classroom, energy):
-
         m = len(classroom)
         n = len(classroom[0])
 
-        litter = {}
-        start = None
+        sx = sy = -1
+        litter_id = [[-1] * n for _ in xrange(m)]
+        litter_count = 0
 
-        # Find start and assign IDs to litter
-        for r in range(m):
-            for c in range(n):
-                if classroom[r][c] == 'S':
-                    start = (r, c)
+        for i in xrange(m):
+            for j in xrange(n):
+                if classroom[i][j] == 'S':
+                    sx, sy = i, j
+                elif classroom[i][j] == 'L':
+                    litter_id[i][j] = litter_count
+                    litter_count += 1
 
-                elif classroom[r][c] == 'L':
-                    litter[(r, c)] = len(litter)
+        full_mask = (1 << litter_count) - 1
 
-        # All litter collected
-        target = (1 << len(litter)) - 1
-
-        if target == 0:
+        if full_mask == 0:
             return 0
-
-        # BFS:
-        # (row, col, energy, mask, steps)
-        q = deque()
-        q.append((start[0], start[1], energy, 0, 0))
-
-        # best[(row, col, mask)] = maximum energy seen
-        best = {
-            (start[0], start[1], 0): energy
-        }
-
-        directions = [
-            (-1, 0),
-            (1, 0),
-            (0, -1),
-            (0, 1)
+        best = [
+            [
+                [-1] * (1 << litter_count)
+                for _ in xrange(n)
+            ]
+            for _ in xrange(m)
         ]
 
+        q = deque()
+        q.append((sx, sy, 0, energy, 0))
+        best[sx][sy][0] = energy
+
+        dirs = ((1, 0), (-1, 0), (0, 1), (0, -1))
+
         while q:
+            x, y, mask, cur_energy, step = q.popleft()
 
-            r, c, e, mask, steps = q.popleft()
+            if mask == full_mask:
+                return step
+            if cur_energy == 0:
+                continue
 
-            for dr, dc in directions:
+            next_step = step + 1
 
-                nr = r + dr
-                nc = c + dc
+            for dx, dy in dirs:
+                nx = x + dx
+                ny = y + dy
 
-                # Outside classroom
-                if nr < 0 or nr >= m or nc < 0 or nc >= n:
+                if nx < 0 or nx >= m or ny < 0 or ny >= n:
                     continue
 
-                # Wall
-                if classroom[nr][nc] == 'X':
+                cell = classroom[nx][ny]
+
+                if cell == 'X':
                     continue
 
-                # Moving costs 1 energy
-                new_energy = e - 1
+                next_energy = cur_energy - 1
+                next_mask = mask
 
-                # Collect litter
-                new_mask = mask
-
-                if classroom[nr][nc] == 'L':
-                    new_mask |= (1 << litter[(nr, nc)])
-
-                # If all litter collected
-                if new_mask == target:
-                    return steps + 1
-
-                # Recharge
-                if classroom[nr][nc] == 'R':
-                    new_energy = energy
-
-                # Can't move further without energy
-                if new_energy <= 0:
+                if cell == 'R':
+                    next_energy = energy
+                elif cell == 'L':
+                    idx = litter_id[nx][ny]
+                    next_mask = mask | (1 << idx)
+                if best[nx][ny][next_mask] >= next_energy:
                     continue
 
-                state = (nr, nc, new_mask)
-
-                # If we've already reached this state
-                # with equal or greater energy, this path
-                # can never be better.
-                if state in best and best[state] >= new_energy:
-                    continue
-
-                # This is a better state
-                best[state] = new_energy
-
-                q.append(
-                    (nr, nc, new_energy, new_mask, steps + 1)
-                )
+                best[nx][ny][next_mask] = next_energy
+                q.append((nx, ny, next_mask, next_energy, next_step))
 
         return -1

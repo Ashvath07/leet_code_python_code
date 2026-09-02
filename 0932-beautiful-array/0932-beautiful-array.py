5652e4489d43043cf47026d91125e448823a56fe
ASHVATH M <ashvathmurali2@gmail.com>
2026-09-02 09:48:53 +0530

    Time: 0 ms (100%), Space: 12.5 MB (43.84%) - LeetHub

--- 0932-beautiful-array/0932-beautiful-array.py
+++ 0932-beautiful-array/0932-beautiful-array.py
@@ -1,15 +1,18 @@
-class Solution(object):
+class Solution:
     def beautifulArray(self, n):
-        memo = {1 : [1]}
-        def generate(size):
-            if size in memo:
-                return memo[size]
-            odds = []
-            evens = []
-            for x in generate((size + 1) // 2):
-                odds.append(2 * x - 1)
-            for x in generate(size // 2):
-                evens.append(2 * x)
-            memo[size] = odds + evens
-            return memo[size]
-        return generate(n)
+        ans = [1]
+
+        while len(ans) < n:
+            new = []
+
+            for x in ans:
+                if 2 * x - 1 <= n:
+                    new.append(2 * x - 1)
+
+            for x in ans:
+                if 2 * x <= n:
+                    new.append(2 * x)
+
+            ans = new
+
+        return ans

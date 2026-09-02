67e4bd9a7eef24220976aded5d938ee87d87e303
ASHVATH M <ashvathmurali2@gmail.com>
2026-09-02 10:31:26 +0530

    Time: 65 ms (78.57%), Space: 13.7 MB (25.51%) - LeetHub

--- 0481-magical-string/0481-magical-string.py
+++ 0481-magical-string/0481-magical-string.py
@@ -1,19 +1,29 @@
 class Solution(object):
-    def magicalString(self, n):
-        if n<=3:
-            return 1
-        else:
-            s="122"
-            i=len(s)-1
-            while True:
-                if s[-1]=='2':
-                    s=s+('1'*int(s[i]))
 
-                else:
-                    s=s+('2'*int(s[i]))
-                if len(s)>=n:
-                    break
-                i=i+1
-            s=s[:n]
-            return s.count('1')
-        
+    def magicalString(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n == 0:
+            return 0
+        if n <= 3:
+            return 1
+
+        # Initial prefix of the magical string
+        s = [1, 2, 2]
+        head = 2  # Pointer indicating group length for next element(s)
+        num = 1  # Next number to append (alternates between 1 and 2)
+
+        while len(s) < n:
+            # Append `num` repeated `s[head]` times
+            s.extend([num] * s[head])
+
+            # Alternate num between 1 and 2 (1 ^ 3 = 2, 2 ^ 3 = 1)
+            num ^= 3
+
+            # Move pointer to the next group length instruction
+            head += 1
+
+        # Count 1s in the first n elements
+        return s[:n].count(1)

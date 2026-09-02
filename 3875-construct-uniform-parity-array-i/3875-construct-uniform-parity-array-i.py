29a627d544468c6c5987f5d921a71ecc24f07d26
ASHVATH M <ashvathmurali2@gmail.com>
2026-09-02 09:08:17 +0530

    Time: 3 ms (16.25%), Space: 12.3 MB (65%) - LeetHub

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def uniformArray(self, nums1):
+        # nums2 = [0]*len(nums1)
+        # for i in range(len(nums1)):
+        #     nums2[i]=nums1[i]-nums1[i-1]
+        #     nums2[i-1] = nums1[i-1]
+        # return nums2
+        return True

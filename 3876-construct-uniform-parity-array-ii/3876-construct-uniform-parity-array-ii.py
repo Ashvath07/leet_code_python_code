941bddc2befdb0153b286f281b01779a4aaa5932
ASHVATH M <ashvathmurali2@gmail.com>
2026-09-03 21:46:47 +0530

    Time: 23 ms (76.92%), Space: 21.7 MB (61.54%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.py
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.py
@@ -1,3 +1,6 @@
 class Solution(object):
     def uniformArray(self, nums1):
-        return not (min(nums1) ^ reduce(or_ , nums1)) & 1
+        min_val = min(nums1)
+        if min_val % 2 != 0:
+            return True
+        return all(x % 2 == 0 for x in nums1)

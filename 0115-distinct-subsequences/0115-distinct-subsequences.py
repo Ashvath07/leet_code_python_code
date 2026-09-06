b8ea9ab5743580499cfc0d405ff49dc582d7c1e3
ASHVATH M <ashvathmurali2@gmail.com>
2026-09-06 20:54:34 +0530

    Time: 521 ms (49.89%), Space: 69.3 MB (19.05%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.py
+++ 0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -1,24 +1,13 @@
 class Solution(object):
     def numDistinct(self, s, t):
-        memo = {}
-        def findOccurrences(i, j):
-            s_len = len(s)
-            t_len = len(t)
-            if j == t_len:
-                return 1          
-            if i == s_len or s_len - i < t_len - j:
-                return 0
-            if (i, j) in memo:
-                return memo[(i, j)]
-            add = findOccurrences(i + 1, j)
-            if s[i] == t[j]:
-                add += findOccurrences(i + 1, j + 1)          
-            memo[(i, j)] = add
-            return add        
-        return findOccurrences(0, 0)
-
-
-
-
-            
-        
+        m,n=len(s),len(t)
+        dp=[[0]*(n+1) for _ in range(m+1)]
+        for i in range(m+1):
+            dp[i][0]=1      
+        for i in range(1,m+1):
+            for j in range(1,n+1):
+                if s[i-1]==t[j-1]:
+                    dp[i][j]=dp[i-1][j-1]+dp[i-1][j]
+                else:
+                    dp[i][j]=dp[i-1][j]
+        return dp[m][n]

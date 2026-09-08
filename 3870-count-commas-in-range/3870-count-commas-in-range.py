eafc9678c4900886a86d989e36a8189eae1b7c13
ASHVATH M <ashvathmurali2@gmail.com>
2026-09-08 07:34:43 +0530

    Time: 0 ms (100%), Space: 12.3 MB (93.2%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.py
+++ 3870-count-commas-in-range/3870-count-commas-in-range.py
@@ -1,8 +1,7 @@
 class Solution(object):
     def countCommas(self, n):
-        count =0
-        for i in range(1,n+1):
-            if i >=1000:
-                count +=1
-        return count
+        if n <1000:
+            return 0
+        else:
+            return n-999
         

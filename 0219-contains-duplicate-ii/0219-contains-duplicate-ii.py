757e35c690e2563030d650b6a9a2a7539f2eec23
ASHVATH M <ashvathmurali2@gmail.com>
2026-09-11 08:30:04 +0530

    Time: 61 ms (38.32%), Space: 29.4 MB (16.56%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def containsNearbyDuplicate(self, nums, k):
+        freq={}
+        for i in range(len(nums)):
+            if nums[i] in freq and abs(i-freq[nums[i]])<=k:
+                return True
+            freq[nums[i]] = i
+        return False

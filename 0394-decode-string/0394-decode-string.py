96a6db5136a01a855573f7535e08a3d5672484ae
ASHVATH M <ashvathmurali2@gmail.com>
2026-09-13 10:38:17 +0530

    Time: 0 ms (100%), Space: 12.4 MB (56.59%) - LeetHub

--- 0394-decode-string/0394-decode-string.py
+++ 0394-decode-string/0394-decode-string.py
@@ -1,34 +1,23 @@
-class Solution:
+class Solution(object):
     def decodeString(self, s):
-        # Initialize the stack and the current string
-        stack = []
-        curr_str = ""
-        # Initialize the current number to 0
-        curr_num = 0
-        
-        # Iterate through each character of the string
-        for c in s:
-            # If the character is a digit, update the current number
-            if c.isdigit():
-                curr_num = curr_num * 10 + int(c)
-            # If the character is an opening bracket, push the current number and current string onto the stack
-            elif c == "[":
-                stack.append(curr_num)
-                stack.append(curr_str)
-                # Reset the current number and current string
-                curr_num = 0
-                curr_str = ""
-            # If the character is a closing bracket, repeat the popped characters and push the result back onto the stack
-            elif c == "]":
-                prev_str = stack.pop()
-                prev_num = stack.pop()
-                curr_str = prev_str + curr_str * prev_num
-            # If the character is a letter, append it to the current string
+        stack1 = []
+        num = 0
+        current = ''
+        for i in range(len(s)):
+            if s[i].isdigit():
+                num = num * 10 + int(s[i])
+
+            elif s[i] == '[':
+                stack1.append((num, current))
+                num = 0
+                current = ''
+
+            elif s[i] == ']':
+                current = stack1[-1][1] + current * stack1[-1][0]
+                stack1.pop()
+                # repeat, previous = stack1.pop()
+                # current = previous + current * repeat
+
             else:
-                curr_str += c
-        
-        # Pop any remaining characters from the stack and concatenate them to the final result
-        while stack:
-            curr_str = stack.pop() + curr_str
-        
-        return curr_str
+                current += s[i]
+        return current

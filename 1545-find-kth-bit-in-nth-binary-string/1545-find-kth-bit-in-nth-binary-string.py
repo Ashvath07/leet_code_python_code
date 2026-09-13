424f929b2377fd7cbf1e703fa1ee7640760a1af3
ASHVATH M <ashvathmurali2@gmail.com>
2026-09-13 10:44:08 +0530

    Time: 0 ms (100%), Space: 12.5 MB (48.98%) - LeetHub

---
+++ 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def findKthBit(self, n, k):
+        if n == 1:
+            return "0"
+        length =(1<<n)-1
+        mid = length//2+1
+        if k == mid:
+            return '1'
+        if k < mid:
+            return self.findKthBit(n-1,k)
+        return '1' if self.findKthBit(n-1,length - k+1) == "0" else "0"
